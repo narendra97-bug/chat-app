6fbe203bb3d6bc448493153770674ac0f5e2cce9
narendra97-bug <56558153+narendra97-bug@users.noreply.github.com>
2021-09-03 13:27:18 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,42 @@
+const socket=io('http://localhost:8000')
+
+const form=document.getElementById('send-container')
+const massageInput=document.getElementById('messageinp')
+const messageContainer=document.querySelector('.container')
+var audio= new Audio('tone.mp3');
+
+
+const append=(message,position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    
+    if(position=='left'){
+
+        audio.play();
+    }
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=messageinp.value;
+    append(`you:${message}`,'right');
+    socket.emit('send',message);
+    messageinp.value="";
+})
+const name=prompt('enter your name to join');
+socket.emit('new-user-joined',name);
+
+socket.on('user-joined',name=>{
+    append(`${name} joined the chat.`,'left')
+})
+
+socket.on('receive',data=>{
+    append(`${data.name}:${data.message}`,'left')
+})
+
+socket.on('left',name=>{
+    append(`${name} left the chat.`,'left')
+})
